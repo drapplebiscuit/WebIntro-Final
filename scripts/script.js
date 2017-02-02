491c6f9eb4106d0d08c60986e9b53e64c20447d2
Colin Misbach <CMisbach@student.neumont.edu>
2017-02-02 13:10:53 -0700

    slide show | updated logo

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,18 @@
-$((document).ready(){
-  
+$(document).ready(function(){
+    var imgs = ["images/logo.png", "images/me.png"];
+    var imgCount = 0;
+    $("#websum").append('<img class="display" src="'+ imgs[0] +'" width="400"/>');
+    
+    setInterval(changePic, 3000);
+    
+    function changePic() {
+        imgCount++;
+        
+        if (imgCount == imgs.length) {
+            imgCount = 0;
+        }
+        
+        $("#websum").html("");
+        $("#websum").append('<img class="display" src="'+ imgs[imgCount] +'" width="400"/>');
+    }
   });
